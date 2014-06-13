7f303210adbab7aea1808745758eb37451c41fd5
glonsberry <gardnerlonsberry@gmail.com>
2014-06-13 13:50:15 -0400

    removed --warning from .spec. testing suite functional

--- app/models/twixer.rb
+++ app/models/twixer.rb
@@ -1,6 +1,5 @@
 class Twixer < ActiveRecord::Base
   authenticates_with_sorcery!
-  has_many :twix_notes
 
   validates_presence_of :password, on: :create
   validates_presence_of :email, on: :create
