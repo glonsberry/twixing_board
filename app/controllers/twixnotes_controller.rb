3bc8c0afb79bfe89ebae9bcaef06bfa9aa8bc599
wakelank <wakelank@gmail.com>
2014-06-16 11:34:13 -0400

    implements twixnote fetch, delete

--- app/controllers/twixnotes_controller.rb
+++ app/controllers/twixnotes_controller.rb
@@ -9,7 +9,7 @@ before_action :require_login
       format.json { render :json =>new_twixnote }
       format.html { redirect_to show_path }
     end
-    twixingboard = Twixingboard.find(params[:twixingboard_id])
+    twixingboard = Twixingboard.find(params[:id])
     twixingboard.twixnotes << new_twixnote
     
   end
@@ -22,20 +22,22 @@ before_action :require_login
     respond_to do |format|
       format.json { render :json =>{ deleted: delete_id } } 
     end
-    
-
   end
 
   def search
-    twixnote = Twixnote.new
-    twixnote.name = params[:search_term]
-    twixnote.frequency = twixnote.find_frequency(twixnote.name)
-    twixnote.save()
+    new_twixnote = Twixnote.new
+    new_twixnote.name = params[:search_term]
+    new_twixnote.frequency = new_twixnote.find_frequency(new_twixnote.name)
+    new_twixnote.save()
+    twixingboard = Twixingboard.find(params[:id])
+    twixingboard.twixnotes << new_twixnote
 
     respond_to do |format|
-      format.json { render :json => twixnote }
+      format.json { render :json => new_twixnote }
     end
   end
+
+ 
 
 
 
